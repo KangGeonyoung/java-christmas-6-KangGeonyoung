937c73f7e4c6b8cf2061613f3e2c72ec8696620e
KangGeonyoung <kky6335@gmail.com>
2023-11-11 17:51:28 +0900

    feat(Date): 방문 날짜가 숫자인지 검사 - isValidDate(String input): 날짜가 숫자인지 검사

--- src/main/java/christmas/domain/model/Date.java
+++ src/main/java/christmas/domain/model/Date.java
@@ -2,6 +2,15 @@ package christmas.domain.model;
 
 public class Date {
 
+    public void isValidDate(String input) {
+        try {
+            int date = Integer.parseInt(input);
+            isValidRange(date);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
     private void isValidRange(int date) {
         if (date < 1 || date > 31) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
