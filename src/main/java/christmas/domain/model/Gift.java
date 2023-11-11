edf45f1722e073a9390f5a60ac3e15a661f0a611
KangGeonyoung <kky6335@gmail.com>
2023-11-12 00:02:07 +0900

    feat(Gift): 증정 선물 받았는지 확인 기능 - getGiftCount(): 받은 증정 선물의 개수 리턴

--- src/main/java/christmas/domain/model/Gift.java
+++ src/main/java/christmas/domain/model/Gift.java
@@ -2,10 +2,18 @@ package christmas.domain.model;
 
 public class Gift {
 
+    private static int giftCount;
+
     public static boolean provideGift(int totalPrice) {
         if (totalPrice >= 120000) {
+            giftCount = 1;
             return true;
         }
+        giftCount = 0;
         return false;
+    }
+
+    public static int getGiftCount() {
+        return giftCount;
     }
 }
