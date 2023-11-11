b140e6aa0a3fd2d164882d52e304c54f10899068
KangGeonyoung <kky6335@gmail.com>
2023-11-11 17:50:35 +0900

    feat(Date): 방문 날짜가 1-31 범위 내에 있는지 검사 - isValidRange(int date): 날짜가 1-31 범위 내에 있는지 검사

---
+++ src/main/java/christmas/domain/model/Date.java
@@ -0,0 +1,10 @@
+package christmas.domain.model;
+
+public class Date {
+
+    private void isValidRange(int date) {
+        if (date < 1 || date > 31) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+}
