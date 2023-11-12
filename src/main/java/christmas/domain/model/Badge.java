60b6fd703d74270ab73633f2b9e89f8d8575164f
KangGeonyoung <kky6335@gmail.com>
2023-11-12 17:53:26 +0900

    feat(Badge): 배지 부여 순서 변경 - badgeEvent(int totalDiscount): 총혜택 금액이 높은 순서부터 배지 부여

--- src/main/java/christmas/domain/model/Badge.java
+++ src/main/java/christmas/domain/model/Badge.java
@@ -5,17 +5,18 @@ public class Badge {
     private final String STAR = "별";
     private final String TREE = "트리";
     private final String SANTA = "산타";
+    private final String NONE = "없음";
 
     public String badgeEvent(int totalDiscount) {
-        if (totalDiscount >= 5000) {
-            return STAR;
+        if (totalDiscount >= 20000) {
+            return SANTA;
         }
         if (totalDiscount >= 10000) {
             return TREE;
         }
-        if (totalDiscount >= 20000) {
-            return SANTA;
+        if (totalDiscount >= 5000) {
+            return STAR;
         }
-        return "";
+        return NONE;
     }
 }
