362b533f14cab308773fee13b2a1acab3f310620
KangGeonyoung <kky6335@gmail.com>
2023-11-12 17:59:22 +0900

    feat(OutputView): 프로모션 시작 인사 - printHello(): 프로모션 시작 인사 기능

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,18 +1,26 @@
 package christmas.view;
 
+import christmas.domain.model.Event;
+import christmas.domain.model.Price;
+
 import java.util.Map;
 
 public class OutputView {
 
-    public void printDate(int date) {
+    public static void printHello() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public static void printDate(int date) {
         System.out.println("12월 " + date + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
     }
 
-    public void printMenu(Map<String, Integer> orderedMenu) {
+    public static void printMenu(Map<String, Integer> orderedMenu) {
         System.out.println();
         System.out.println("<주문 메뉴>");
         orderedMenu.forEach((menu, count) -> {
             System.out.println(menu + " " + count + "개");
         });
     }
+
 }
