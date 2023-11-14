be621f8f037bf1d78d919d38f23ff69ffd23d69e
KangGeonyoung <kky6335@gmail.com>
2023-11-14 22:59:11 +0900

    refactor(Date): 메서드 접근 제어자 변경

--- src/main/java/christmas/domain/model/Date.java
+++ src/main/java/christmas/domain/model/Date.java
@@ -21,13 +21,13 @@ public class Date {
         }
     }
 
-    private void isValidRange(int date) {
+    public void isValidRange(int date) {
         if (date < 1 || date > 31) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_DATE.getErrorMessage());
         }
     }
 
-    public static boolean isWeekend(int date) {
+    public boolean isWeekend(int date) {
         List<Integer> weekend = new ArrayList<>(List.of(1, 2, 8, 9, 15, 16, 22, 23, 29, 30));
         if (weekend.contains(date)) {
             return true;
@@ -35,7 +35,7 @@ public class Date {
         return false;
     }
 
-    public static boolean isSpecialDay(int date) {
+    public boolean isSpecialDay(int date) {
         List<Integer> specialDay = new ArrayList<>(List.of(3, 10, 17, 24, 25, 31));
         if (specialDay.contains(date)) {
             return true;
@@ -43,7 +43,7 @@ public class Date {
         return false;
     }
 
-    public static boolean isChristmasDay(int date) {
+    public boolean isChristmasDay(int date) {
         if (date >= 1 && date <= 25) {
             return true;
         }
