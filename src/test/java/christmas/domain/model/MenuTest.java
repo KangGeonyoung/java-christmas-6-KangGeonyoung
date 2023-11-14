cd7720757d1ba32b17b1d816721c890043ee08c1
KangGeonyoung <kky6335@gmail.com>
2023-11-14 23:14:48 +0900

    test(MenuTest): 특정 케이스에 대한 테스트 - 메뉴_입력값_중복메뉴 - 메뉴_입력값_존재하지않는_메뉴_주문할경우 - 메뉴_입력값_음료만_주문할경우

--- src/test/java/christmas/domain/model/MenuTest.java
+++ src/test/java/christmas/domain/model/MenuTest.java
@@ -68,4 +68,31 @@ class MenuTest {
             menuSystem.isValidMenuCount(orderedMenu);
         });
     }
+
+    @Test
+    void 메뉴_입력값_중복메뉴() {
+        String input = "타파스-1,타파스-3";
+        assertThrows(IllegalArgumentException.class, () -> {
+            Map<String, Integer> orderedMenu = menuSystem.isValidMenu(input);
+        });
+    }
+
+    @Test
+    void 메뉴_입력값_존재하지않는_메뉴_주문할경우() {
+        String input = "펩시콜라-3,타파스-15";
+        assertThrows(IllegalArgumentException.class, () -> {
+            Map<String, Integer> orderedMenu = menuSystem.convertOrderedMenu(input);
+            menuSystem.isWithinMenu(orderedMenu);
+        });
+    }
+
+    @Test
+    void 메뉴_입력값_음료만_주문할경우() {
+        String input = "제로콜라-15,레드와인-1";
+        assertThrows(IllegalArgumentException.class, () -> {
+            Map<String, Integer> orderedMenu = menuSystem.convertOrderedMenu(input);
+            menuSystem.isValidMenuCount(orderedMenu);
+            menuSystem.isOnlyBeverage(orderedMenu);
+        });
+    }
 }
